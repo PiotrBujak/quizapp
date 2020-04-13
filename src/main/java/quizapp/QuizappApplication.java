f9e0c215030edbb286bb64c381c2bc19f84d2af9
PiotrBujak <Piotrus.bujak@gmail.com>
2020-04-13 15:55:14 +0200

    aplikacja korzysta z appliacation.properties button solve test ready

--- src/main/java/quizapp/QuizappApplication.java
+++ src/main/java/quizapp/QuizappApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @SpringBootApplication
+@PropertySource("classpath:config.properties")
 public class QuizappApplication {
 
     public static void main(String[] args) {
